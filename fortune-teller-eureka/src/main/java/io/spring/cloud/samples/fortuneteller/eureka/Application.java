6049d0df4669a19f06444911d8038dde8d435a71
Matt Stine <mstine@gopivotal.com>
2015-05-21 11:20:27 -0700

    rename eureka module and packaging

--- fortune-teller-eureka/src/main/java/io/spring/cloud/samples/fortuneteller/eureka/Application.java
+++ fortune-teller-eureka/src/main/java/io/spring/cloud/samples/fortuneteller/eureka/Application.java
@@ -1,4 +1,4 @@
-package io.pivotal.nfjsworkshop.eureka;
+package io.spring.cloud.samples.fortuneteller.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
