b7bc38966f513712f303101143fa36e786776522
Matt Stine <mstine@gopivotal.com>
2015-05-21 11:47:38 -0700

    refactor CF manifests

--- scripts/create_services.sh
+++ scripts/create_services.sh
@@ -1,3 +1,3 @@
-cf cups config-service -p '{"uri":"http://config-server-fluxional-suttee.cfapps.io"}'
-cf cups service-registry -p '{"uri":"http://eureka-unprevalent-toper.cfapps.io"}'
-cf cs elephantsql turtle fortunes-db
+cf cups config-service -p '{"uri":"http://config-server-unvariegated-capitulary.cfapps.io/"}'
+cf cups service-registry -p '{"uri":"http://eureka-psoatic-cirrocumulus.cfapps.io/"}'
+cf cs elephantsql turtle fortune-db
