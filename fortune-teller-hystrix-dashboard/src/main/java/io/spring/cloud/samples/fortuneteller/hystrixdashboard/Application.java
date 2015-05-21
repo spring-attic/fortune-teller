b6404d300f1ddf32849c96ec15de484ab9067dfd
Matt Stine <mstine@gopivotal.com>
2015-05-21 11:28:43 -0700

    rename hystrixDashboard module and packaging

--- fortune-teller-hystrix-dashboard/src/main/java/io/spring/cloud/samples/fortuneteller/hystrixdashboard/Application.java
+++ fortune-teller-hystrix-dashboard/src/main/java/io/spring/cloud/samples/fortuneteller/hystrixdashboard/Application.java
@@ -1,4 +1,4 @@
-package io.pivotal.nfjsworkshop.hystrixdashboard;
+package io.spring.cloud.samples.fortuneteller.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
