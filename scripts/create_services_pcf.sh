9fac8562aada23b5d9987181bf03f9ce84a9618b
Matt Stine <mstine@gopivotal.com>
2015-06-02 14:08:36 -0500

    update manifests and configuration scripts to work on PCF with SCS

---
+++ scripts/create_services_pcf.sh
@@ -0,0 +1,4 @@
+cf cs p-mysql 100mb-dev fortune-db
+cf cs p-config-server standard config-server
+cf cs p-service-registry standard service-registry
+cf cs p-circuit-breaker-dashboard standard circuit-breaker
