dc982616837a17108d0b0fe5c46e1a372cb84eb8
Matt Stine <mstine@gopivotal.com>
2015-07-28 19:55:30 -0400

    deltas from master to make work with spring-cloud-services master

--- fortune-teller-fortune-service/src/main/java/io/spring/cloud/samples/fortuneteller/fortuneservice/Application.java
+++ fortune-teller-fortune-service/src/main/java/io/spring/cloud/samples/fortuneteller/fortuneservice/Application.java
@@ -1,5 +1,6 @@
 package io.spring.cloud.samples.fortuneteller.fortuneservice;
 
+import io.pivotal.springcloud.ssl.CloudFoundryCertificateTruster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class Application {
 
     public static void main(String[] args) {
+        CloudFoundryCertificateTruster.trustApiCertificate();
         SpringApplication.run(Application.class, args);
     }
 }
--- fortune-teller-ui/src/main/java/io/spring/cloud/samples/fortuneteller/ui/Application.java
+++ fortune-teller-ui/src/main/java/io/spring/cloud/samples/fortuneteller/ui/Application.java
@@ -1,5 +1,6 @@
 package io.spring.cloud.samples.fortuneteller.ui;
 
+import io.pivotal.springcloud.ssl.CloudFoundryCertificateTruster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class Application {
 
     public static void main(String[] args) {
+        CloudFoundryCertificateTruster.trustApiCertificate();
         SpringApplication.run(Application.class, args);
     }
 }
