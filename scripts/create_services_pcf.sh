40c095a9d08e287d88208f5672effb01cabbd523
Roy Clarkson <rclarkson@pivotal.io>
2016-07-13 17:39:53 -0500

    Set git uri and searchPaths params in the create_services_pcf script

--- scripts/create_services_pcf.sh
+++ scripts/create_services_pcf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cf cs p-mysql 100mb-dev fortunes-db
-cf cs p-config-server standard config-server -c '{"git": { "uri": "https://github.com/spring-cloud-samples/fortune-teller" } }'
+cf cs p-config-server standard config-server -c '{"git": { "uri": "https://github.com/spring-cloud-samples/fortune-teller", "searchPaths": "configuration" } }'
 cf cs p-service-registry standard service-registry
 cf cs p-circuit-breaker-dashboard standard circuit-breaker-dashboard
